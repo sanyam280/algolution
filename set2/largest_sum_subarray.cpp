19b168c8f51e3ff437e6f6991d7a65aabc55bca1
sanyam280 <122448993+sanyam280@users.noreply.github.com>
2024-12-01 23:41:41 +0530

    naiveapproach largest_sum_subarray.cpp

--- set2/largest_sum_subarray.cpp
+++ set2/largest_sum_subarray.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+//naive
+int largest_sum_subarray(int *arr, int n){
+    int max_sum = arr[0], sum = 0; 
     
+    for(int i = 0; i < n; i++){
+        sum = 0;
+        for(int j = i; j < n; j++){ 
+            sum += arr[j];
+            if(sum > max_sum) max_sum = sum;
+        }
+    }
+    return max_sum;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int * arr = new int[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << largest_sum_subarray(arr, n);
 }
